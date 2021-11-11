fb1bfd7af89e8aff7d923e2aa27fb69a7419f70c
JoseLRM <joselrm890@gmail.com>
2021-11-11 23:31:01 +0100

    XML parser

--- src/serialize.c
+++ src/serialize.c
@@ -10,13 +10,6 @@
 
 #include "Hosebase/external/stbi_lib.h"
 
-#define MINIZ_USE_UNALIGNED_LOADS_AND_STORES 1
-#define MINIZ_LITTLE_ENDIAN 1
-#define MINIZ_HAS_64BIT_REGISTERS 1
-#define MINIZ_NO_STDIO
-
-#include "Hosebase/external/miniz/miniz.c"
-
 b8 load_image(const char* filepath_, void** pdata, u32* width, u32* height)
 {
 	char filepath[FILE_PATH_SIZE];
@@ -968,6 +961,7 @@ static b8 model_load_obj(ModelInfo* model_info, const char* filepath, const char
 }
 
 ///////////////////////////////////////////////////////////// FBX FORMAT ///////////////////////////////////////////////////
+/*
 
 static void read_fbx_node(Deserializer* s)
 {
@@ -1139,6 +1133,297 @@ static b8 model_load_fbx(ModelInfo* model_info, const char* filepath, char* it, 
 	deserializer_end_buffer(&s);
 	return TRUE;
 }
+*/
+
+////////////////////////////////////////// DAE FORMAT /////////////////////////////////////
+
+typedef struct {
+
+	b8 corrupted;
+	const char* data;
+	u32 size;
+
+	i32 level;
+	const char* begin;
+	const char* end;
+
+} XMLElement;
+
+inline b8 xml_string_equals(const char* str0, const char* str1)
+{
+	while (*str0 != '\0' && *str1 != '\0') {
+
+		if (*str0 == '>' || *str0 == '/' || *str0 == ' ') {
+			str0 = " ";
+		}
+		if (*str1 == '>' || *str1 == '/' || *str1 == ' ') {
+			str1 = " ";
+		}
+
+		if (*str0 != *str1) {
+			return FALSE;
+		}
+
+		++str0;
+		++str1;
+	}
+
+	return *str0 == *str1;
+}
+
+inline const char* xml_exit_tag(const char* c)
+{
+	while (*c != '\0' && *c != '>') {
+
+		if (*c == '/' && *(c + 1) == '>') {
+			++c;
+			break;
+		}
+
+		++c;
+	}
+
+	if (*c == '\0') return c;
+	
+	return c + 1;
+}
+
+inline const char* xml_find_end(const char* begin)
+{
+	const char* c = begin + 1;
+	const char* name_begin = c;
+	i32 level = 0;
+
+	// Exit from tag
+	{
+		while (*c != '\0' && *c != '>') {
+
+			if (*c == '/' && *(c + 1) == '>') {
+				break;
+			}
+
+			++c;
+		}
+
+		if (*c == '\0') return NULL;
+		if (*c == '/') {
+			return c + 2;
+		}
+		else {
+			++c;
+		}
+	}
+
+	// Find close tag
+	{
+		while (1) {
+			while (*c != '\0' && *c != '<') {
+
+				if (*c == '/' && *(c + 1) == '>') {
+					level--;
+					++c;
+				}
+
+				++c;
+			}
+
+			if (*c == '\0') 
+				return NULL;
+
+			++c;
+
+			if (*c == '/') {
+
+				++c;
+
+				if (level == 0 && xml_string_equals(name_begin, c)) {
+
+					while (*c != '\0' && *c != '>') {
+						++c;
+					}
+
+					if (*c == '>') {
+
+						return c + 1;
+					}
+					else return NULL;
+				}
+				else {
+					level--;
+				}
+			}
+			else level++;
+		}
+	}
+
+	return begin;
+}
+
+static XMLElement xml_begin(const char* data, u32 size)
+{
+	XMLElement e;
+	e.data = data;
+	e.size = size;
+	e.corrupted = FALSE;
+	e.level = 0;
+
+	const char*c = line_jump_spaces(e.data);
+
+	while (!e.corrupted) {
+
+		if (*c== '<') {
+
+			e.begin = c;
+
+			++c;
+
+			if (*c == '?') {
+
+				while (*c != '>' && *c != '\0')
+					c++;
+
+				if (*c == '>') {
+					++c;
+				}
+				else {
+					e.corrupted = TRUE;
+				}
+			}
+			else {
+
+				const char* end = xml_find_end(e.begin);
+
+				if (end) {
+					e.end = end;
+					break;
+				}
+				else {
+					e.corrupted;
+				}
+			}
+		}
+		else if (*c == '\n' || *c == ' ' || *c == '\r' || *c == '\t') {
+			++c;
+		}
+		else {
+			e.corrupted = TRUE;
+		}
+	}
+
+	return e;
+}
+
+static u32 xml_name(const XMLElement* e, char* buffer, u32 buffer_size)
+{
+	const char* begin = e->begin + 1;
+	const char* end = begin;
+
+	while (*end != '\0' && *end != '/' && *end != ' ' && *end != '>') {
+		++end;
+	}
+
+	u32 size = end - begin;
+
+	if (buffer_size == 0)
+		return size;
+
+	u32 copy = SV_MIN(size, buffer_size - 1);
+
+	memory_copy(buffer, begin, copy);
+	buffer[copy] = '\0';
+
+	return size - copy;
+}
+
+inline b8 xml_string_equals_to_normal(const char* xml_str, const char* normal)
+{
+	while (*xml_str != '\0' && *normal != '\0') {
+
+		if (*xml_str == '/' || *xml_str == '>' || *xml_str == ' ') {
+			xml_str = "";
+		}
+
+		if (*xml_str != *normal) {
+			return FALSE;
+		}
+
+		++xml_str;
+		++normal;
+	}
+	return TRUE;
+}
+
+static b8 xml_enter_child(XMLElement* e, const char* name)
+{
+	const char* c = xml_exit_tag(e->begin);
+
+	while (1) {
+
+		while (*c != '\0' && *c != '<') {
+			++c;
+		}
+
+		if (*c == '\0') {
+			e->corrupted = TRUE;
+			return FALSE;
+		}
+
+		++c;
+
+		const char* begin = c - 1;
+		const char* end = xml_find_end(begin);
+
+		if (end == NULL) {
+			e->corrupted = TRUE;
+			return FALSE;
+		}
+
+		if (xml_string_equals_to_normal(c, name)) {
+
+			e->begin = begin;
+			e->end = end;
+			e->level++;
+			break;
+		}
+		else {
+
+			c = end;
+		}
+	}
+
+	return TRUE;
+
+corrupted:
+	e->corrupted = TRUE;
+	return FALSE;
+}
+
+static b8 xml_next(XMLElement* e)
+{
+
+}
+
+static b8 model_load_dae(ModelInfo* model_info, const char* filepath, char* it, u32 file_size)
+{
+	XMLElement root = xml_begin(it, file_size);
+
+	if (root.corrupted) {
+		SV_LOG_ERROR("Corrupted .dae\n");
+		return FALSE;
+	}
+
+	XMLElement geometries = root;
+
+	if (xml_enter_child(&geometries, "library_geometries")) {
+
+	}
+	else {
+		SV_LOG_ERROR("Library geometries not found in .dae\n");
+		return FALSE;
+	}
+
+	return TRUE;
+}
 
 b8 model_load(ModelInfo* model_info, const char* filepath)
 {
@@ -1154,7 +1439,7 @@ b8 model_load(ModelInfo* model_info, const char* filepath)
 	{
 		const char* supported_formats[] = {
 			".obj",
-			".fbx"
+			".dae"
 		};
 
 		foreach(i, SV_ARRAY_SIZE(supported_formats)) {
@@ -1195,7 +1480,7 @@ b8 model_load(ModelInfo* model_info, const char* filepath)
 			model_load_obj(model_info, filepath, file_data);
 			break;
 		case 1:
-			model_load_fbx(model_info, filepath, file_data, file_size);
+			model_load_dae(model_info, filepath, file_data, file_size);
 			break;
 		}
 	}
