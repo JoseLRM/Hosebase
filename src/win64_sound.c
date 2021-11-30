ec9307d2ac63a0193dde9880b1e9e14563a403d9
JoseLRM <joselrm890@gmail.com>
2021-11-30 23:17:59 +0100

    SoundEngine: clean sin waves

--- src/win64_sound.c
+++ src/win64_sound.c
@@ -12,6 +12,12 @@ typedef struct {
 	LPDIRECTSOUNDBUFFER secondary_buffer;
 
 	u32 buffer_size;
+	u32 samples_per_second;
+	u32 bytes_per_sample;
+	u32 write_sample_latency;
+
+	u32 sample_index;
+	f32 tsin;
 
 } SoundSystemData;
 
@@ -81,14 +87,67 @@ b8 _sound_initialize(u32 samples_per_second, u32 buffer_size)
 		}
 
 		sound->secondary_buffer = secondary_buffer;
-		sound->buffer_size = buffer_size;
 	}
 
-	// TEMP
-	sound->secondary_buffer->lpVtbl->Play(sound->secondary_buffer, 0, 0, DSBPLAY_LOOPING);
+	sound->buffer_size = buffer_size;
+	sound->samples_per_second = samples_per_second;
+	sound->bytes_per_sample = sizeof(i16) * 2;
+	sound->write_sample_latency = samples_per_second / 20;
 
 	return TRUE;
 }
+
+static void fill_sound_buffer(u32 byte_to_lock, u32 bytes_to_write, u32 wave_period, u32 wave_tone)
+{
+	u32 sample_index = sound->sample_index;
+	f32 tsin = sound->tsin;
+
+	void* region0;
+	void* region1;
+	DWORD region0_size, region1_size;
+
+	if (sound->secondary_buffer->lpVtbl->Lock(sound->secondary_buffer, byte_to_lock, bytes_to_write, &region0, &region0_size, &region1, &region1_size, 0) == DS_OK) {
+
+		u32 sample_count = region0_size / sound->bytes_per_sample;
+		i16* samples = region0;
+		foreach(index, sample_count) {
+
+			i64 value = (i32)(sinf(tsin) * (f32)wave_tone);
+
+			*samples++ = value;
+			*samples++ = value;
+
+			sample_index++;
+			tsin += (2.f * PI) / (f32)wave_period;
+		}
+
+		sample_count = region1_size / sound->bytes_per_sample;
+		samples = region1;
+		foreach(index, sample_count) {
+
+			i64 value = (i32)(sinf(tsin) * (f32)wave_tone);
+
+			*samples++ = value;
+			*samples++ = value;
+
+			sample_index++;
+			tsin += (2.f * PI) / (f32)wave_period;
+		}
+
+		sound->secondary_buffer->lpVtbl->Unlock(sound->secondary_buffer, region0, region0_size, region1, region1_size);
+	}
+
+	// Reduce the tsin value to mantain his precision
+	tsin /= 2.f * PI;
+	tsin = tsin - (f32)(i32)tsin;
+	tsin *= 2.f * PI;
+
+	sound->sample_index = sample_index;
+	sound->tsin = tsin;
+}
+
+// TEMP
+#include "Hosebase/input.h"
 
 void _sound_update()
 {
@@ -103,52 +162,36 @@ void _sound_update()
 	}
 	last_update -= rate;*/
 
-	static u32 sample_index = 0;
+	const u32 hz = 250 + (u32)((input_mouse_position().x + 0.5f) * 200.f);
+	const u32 wave_period = sound->samples_per_second / hz;
+	const u32 wave_tone = 20000;
 
-	const i32 hz = 200 + sin(timer_now() * 3.f) * 100.f;
-	const i32 square_wave_period = 48800 / hz;
+	if (core.frame_count == 0) {
+		fill_sound_buffer(0, sound->write_sample_latency * sound->bytes_per_sample, wave_period, wave_tone);
+		sound->secondary_buffer->lpVtbl->Play(sound->secondary_buffer, 0, 0, DSBPLAY_LOOPING);
+	}
 
-	DWORD play_cursor, write_cursor;
+	{
+		DWORD play_cursor, write_cursor;
 
-	if (sound->secondary_buffer->lpVtbl->GetCurrentPosition(sound->secondary_buffer, &play_cursor, &write_cursor) == DS_OK) {
+		if (sound->secondary_buffer->lpVtbl->GetCurrentPosition(sound->secondary_buffer, &play_cursor, &write_cursor) == DS_OK) {
 
-		DWORD byte_to_lock = sample_index * 4 % sound->buffer_size;
-		DWORD bytes_to_write;
+			DWORD byte_to_lock = (sound->sample_index * sound->bytes_per_sample) % sound->buffer_size;
+			DWORD bytes_to_write;
 
-		if (byte_to_lock > play_cursor) {
-			bytes_to_write = sound->buffer_size - byte_to_lock + play_cursor;
-		}
-		else {
-			bytes_to_write = play_cursor - byte_to_lock;
-		}
+			DWORD target_cursor = (play_cursor + (sound->write_sample_latency * sound->bytes_per_sample)) % sound->buffer_size;
 
-		void* region0;
-		void* region1;
-		DWORD region0_size, region1_size;
-
-		if (sound->secondary_buffer->lpVtbl->Lock(sound->secondary_buffer, byte_to_lock, bytes_to_write, &region0, &region0_size, &region1, &region1_size, 0) == DS_OK) {
-
-			u32 sample_count = region0_size / 4;
-			i16* samples = region0;
-			foreach(index, sample_count) {
-
-				i64 value = (sample_index++ / (square_wave_period / 2) % 2) ? 60000 : -60000;
-
-				*samples++ = value;
-				*samples++ = 0;
+			if (byte_to_lock == target_cursor) {
+				bytes_to_write = 0;
+			}
+			else if (byte_to_lock > target_cursor) {
+				bytes_to_write = sound->buffer_size - byte_to_lock + target_cursor;
+			}
+			else {
+				bytes_to_write = target_cursor - byte_to_lock;
 			}
 
-			sample_count = region1_size / 4;
-			samples = region1;
-			foreach(index, sample_count) {
-				
-				i64 value = (sample_index++ / (square_wave_period / 2) % 2) ? 60000 : -60000;
-
-				*samples++ = value;
-				*samples++ = 0;
-			}
-
-			sound->secondary_buffer->lpVtbl->Unlock(sound->secondary_buffer, region0, region0_size, region1, region1_size);
+			fill_sound_buffer(byte_to_lock, bytes_to_write, wave_period, wave_tone);
 		}
 	}
 }
