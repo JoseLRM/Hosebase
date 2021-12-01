55b7d577051494e7fe8221d855203bb986e6fb73
JoseLRM <joselrm890@gmail.com>
2021-12-01 12:35:58 +0100

    Controlling the audio play runtime

--- sound.h
+++ sound.h
@@ -12,5 +12,7 @@ typedef struct {
 b8 audio_load(Audio* audio, const char* filepath);
 void audio_destroy(Audio* audio);
 
+void audio_play();
+
 b8 _sound_initialize(u32 samples_per_second, u32 buffer_size);
 void _sound_close();
--- src/win64_sound.c
+++ src/win64_sound.c
@@ -1,9 +1,16 @@
 #include "Hosebase/sound.h"
 
+#include "Hosebase/serialize.h"
 #include "windows.h"
 #include "DSound.h"
 
 #pragma comment(lib, "Dsound.lib")
+
+typedef struct {
+	Audio* audio;
+	u32 begin_sample_index;
+	u32 end_sample_index;
+} AudioInstance;
 
 typedef struct {
 	
@@ -17,187 +24,14 @@ typedef struct {
 	u32 write_sample_latency;
 
 	u32 sample_index;
-	f32 tsin;
 
 	// TEMP
 	Audio audio;
+	AudioInstance instance;
 
 } SoundSystemData;
 
 static SoundSystemData* sound;
-
-///////////////////////////////// LOAD WAV FILE //////////////////////////////////////////
-
-#include "Hosebase/serialize.h"
-
-#pragma pack(push)
-#pragma pack(1)
-
-#define RIFF_STR(a, b, c, d) (((u32)(a) << 0) | ((u32)(b) << 8) | ((u32)(c) << 16) | ((u32)(d) << 24))
-
-#define RiffStr_RIFF RIFF_STR('R', 'I', 'F', 'F')
-#define RiffStr_WAVE RIFF_STR('W', 'A', 'V', 'E')
-#define RiffStr_fmt RIFF_STR('f', 'm', 't', ' ')
-#define RiffStr_data RIFF_STR('d', 'a', 't', 'a')
-#define RiffStr_fact RIFF_STR('f', 'a', 'c', 't')
-
-typedef struct {
-	u32 riff;
-	u32 file_size;
-	u32 wave;
-} WaveHeader;
-
-typedef struct {
-	u16 format_tag;
-	u16 channel_count;
-	u32 samples_per_second;
-	u32 avg_bytes_per_second;
-	u16 block_align;
-	u16 bits_per_sample;
-	u16 extension_size;
-	u16 valid_bits;
-	u32 channel_mask;
-	u8 sub_format[16];
-} WaveFmt;
-
-#pragma pack(pop)
-
-b8 audio_load(Audio* audio, const char* filepath)
-{
-	Deserializer s;
-	u8* data;
-	u32 size;
-
-	b8 res = TRUE;
-
-	if (file_read_binary(filepath, &data, &size)) {
-
-		deserializer_begin_buffer(&s, data, size);
-
-		WaveHeader header;
-		deserializer_read(&s, &header, sizeof(WaveHeader));
-
-		if (header.riff != RiffStr_RIFF || header.wave != RiffStr_WAVE || header.file_size != size - 8) {
-			SV_LOG_ERROR("That's an invalid .wav format '%s'\n", filepath);
-			goto corrupted;
-		}
-
-		WaveFmt* fmt;
-		b8 read_fmt = FALSE;
-		void* sample_data = NULL;
-		u32 sample_bytes = 0;
-
-		while (s.cursor < size) {
-
-			u32 chunk_id;
-			u32 chunk_size;
-
-			deserialize_u32(&s, &chunk_id);
-			deserialize_u32(&s, &chunk_size);
-
-			u32 begin_offset = s.cursor;
-			
-			switch (chunk_id)
-			{
-
-			case RiffStr_fmt:
-			{
-				fmt = (WaveFmt*)(s.data + s.cursor);
-
-				if (fmt->format_tag == 1) {
-					
-					if (fmt->channel_count < 1 || fmt->channel_count > 2) {
-						SV_LOG_ERROR("Invalid channel count %u in '%s'\n", fmt->channel_count, filepath);
-						break;
-					}
-
-					read_fmt = TRUE;
-				}
-				else {
-					SV_LOG_ERROR("Only the PCM format is supported\n");
-				}
-
-			}
-			break;
-
-			case RiffStr_data:
-			{
-				sample_data = s.data + s.cursor;
-				sample_bytes = chunk_size;
-			}
-			break;
-
-			}
-
-			
-			begin_offset += chunk_size;
-			if (chunk_size & 1)
-				begin_offset++;
-			s.cursor = begin_offset;
-		}
-
-		if (!read_fmt) {
-			SV_LOG_ERROR("A valid fmt block is not found in '%s'\n", filepath);
-			goto corrupted;
-		}
-		if (sample_bytes == 0) {
-			SV_LOG_ERROR("There are no samples in '%s'\n", filepath);
-			goto corrupted;
-		}
-
-		{
-			audio->channel_count = fmt->channel_count;
-
-			u32 sample_count = (sample_bytes / (fmt->bits_per_sample / 8)) / fmt->channel_count; // Per channel sample count
-			u32 per_channel_bytes = sizeof(i16) * sample_count;
-
-			audio->sample_count = sample_count;
-			audio->samples_per_second = fmt->samples_per_second;
-
-			// TODO: Support 8 bits samples
-
-			if (audio->channel_count == 1) {
-				audio->samples[0] = memory_allocate(per_channel_bytes);
-				audio->samples[1] = NULL;
-
-				deserializer_read(&s, audio->samples[0], per_channel_bytes);
-			}
-			else if (audio->channel_count == 2) {
-				u8* mem = memory_allocate(per_channel_bytes * 2);
-				audio->samples[0] = (i16*)mem;
-				mem += per_channel_bytes;
-				audio->samples[1] = (i16*)mem;
-
-				i16* src = (i16*)sample_data;
-				foreach(i, sample_count) {
-
-					audio->samples[0][i] = *src++;
-					audio->samples[1][i] = *src++;
-				}
-			}
-		}
-
-		goto free_data;
-	corrupted:
-		res = FALSE;
-
-	free_data:
-		deserializer_end_file(&s);
-		memory_free(data);
-	}
-	else res = FALSE;
-
-	return res;
-}
-
-void audio_destroy(Audio* audio)
-{
-	if (audio == NULL)
-		return;
-
-	if (audio->samples[0])
-		memory_free(audio->samples[0]);
-}
 
 static void clear_sound_buffer()
 {
@@ -217,55 +51,48 @@ static void clear_sound_buffer()
 	// sound->tsin = tsin;
 }
 
-static void fill_sound_buffer(u32 byte_to_lock, u32 bytes_to_write, u32 wave_period, u32 wave_tone)
+static void fill_sound_buffer(u32 byte_to_lock, u32 bytes_to_write)
 {
 	u32 sample_index = sound->sample_index;
-	f32 tsin = sound->tsin;
 
-	void* region0;
-	void* region1;
-	DWORD region0_size, region1_size;
+	void* regions[2];
+	DWORD region_sizes[2];
 
-	if (sound->secondary_buffer->lpVtbl->Lock(sound->secondary_buffer, byte_to_lock, bytes_to_write, &region0, &region0_size, &region1, &region1_size, 0) == DS_OK) {
+	if (sound->secondary_buffer->lpVtbl->Lock(sound->secondary_buffer, byte_to_lock, bytes_to_write, &regions[0], &region_sizes[0], &regions[1], &region_sizes[1], 0) == DS_OK) {
 
-		u32 sample_count = region0_size / sound->bytes_per_sample;
-		i16* samples = region0;
-		foreach(index, sample_count) {
+		foreach(buffer_index, 2) {
+		
+			u32 sample_count = region_sizes[buffer_index] / sound->bytes_per_sample;
+			i16* samples = regions[buffer_index];
+			foreach(index, sample_count) {
 
-			i16 value = (i16)(sinf(tsin) * (f32)wave_tone);
+				i32 left_value = 0;
+				i32 right_value = 0;
 
-			u32 relative_sample = (u32)(((f32)sample_index / (f32)sound->samples_per_second) * (f32)sound->audio.samples_per_second);
-			*samples++ = sound->audio.samples[0][relative_sample % sound->audio.sample_count];
-			*samples++ = sound->audio.samples[1][relative_sample % sound->audio.sample_count];
+				AudioInstance* inst = &sound->instance;
+				Audio* audio = inst->audio;
 
-			sample_index++;
-			tsin += (2.f * PI) / (f32)wave_period;
+				if (audio != NULL && inst->end_sample_index > sample_index) {
+
+					u32 relative_sample = sample_index - inst->begin_sample_index;
+					relative_sample = (u32)(((f32)relative_sample / (f32)sound->samples_per_second) * (f32)audio->samples_per_second);
+					relative_sample %= audio->sample_count;
+
+					left_value += sound->audio.samples[0][relative_sample];
+					right_value += sound->audio.samples[1][relative_sample];
+				}
+
+				*samples++ = left_value;
+				*samples++ = right_value;
+
+				sample_index++;
+			}
 		}
 
-		sample_count = region1_size / sound->bytes_per_sample;
-		samples = region1;
-		foreach(index, sample_count) {
-
-			i16 value = (i16)(sinf(tsin) * (f32)wave_tone);
-
-			u32 relative_sample = (u32)(((f32)sample_index / (f32)sound->samples_per_second) * (f32)sound->audio.samples_per_second);
-			*samples++ = sound->audio.samples[0][relative_sample % sound->audio.sample_count];
-			*samples++ = sound->audio.samples[1][relative_sample % sound->audio.sample_count];
-
-			sample_index++;
-			tsin += (2.f * PI) / (f32)wave_period;
-		}
-
-		sound->secondary_buffer->lpVtbl->Unlock(sound->secondary_buffer, region0, region0_size, region1, region1_size);
+		sound->secondary_buffer->lpVtbl->Unlock(sound->secondary_buffer, regions[0], region_sizes[0], regions[1], region_sizes[1]);
 	}
 
-	// Reduce the tsin value to mantain his precision
-	tsin /= 2.f * PI;
-	tsin = tsin - (f32)(i32)tsin;
-	tsin *= 2.f * PI;
-
 	sound->sample_index = sample_index;
-	sound->tsin = tsin;
 }
 
 b8 _sound_initialize(u32 samples_per_second, u32 buffer_size)
@@ -345,9 +172,13 @@ b8 _sound_initialize(u32 samples_per_second, u32 buffer_size)
 	sound->write_sample_latency = samples_per_second / 20;
 
 	clear_sound_buffer();
+	sound->secondary_buffer->lpVtbl->Play(sound->secondary_buffer, 0, 0, DSBPLAY_LOOPING);
 
 	return TRUE;
 }
+
+// TEMP
+#include "input.h"
 
 void _sound_update()
 {
@@ -362,13 +193,11 @@ void _sound_update()
 	}
 	last_update -= rate;*/
 
-	const u32 hz = 250 + sinf((f32)timer_now() * 10.f) * 100.f;
-	const u32 wave_period = sound->samples_per_second / hz;
-	const u32 wave_tone = 40000;
-
-	if (core.frame_count == 0) {
-		sound->secondary_buffer->lpVtbl->Play(sound->secondary_buffer, 0, 0, DSBPLAY_LOOPING);
+	if (input_key(Key_S, InputState_Pressed)) {
+		audio_play();
 	}
+
+	// TODO: If the audio instance count is 0 and the sampler_index is high: Decrese sampler_index to avoid precision issues
 
 	{
 		DWORD play_cursor, write_cursor;
@@ -390,7 +219,9 @@ void _sound_update()
 				bytes_to_write = target_cursor - byte_to_lock;
 			}
 
-			fill_sound_buffer(byte_to_lock, bytes_to_write, wave_period, wave_tone);
+			//TODO: Should limit the bytes_to_write value?
+
+			fill_sound_buffer(byte_to_lock, bytes_to_write);
 		}
 	}
 }
@@ -401,4 +232,186 @@ void _sound_close()
 
 		memory_free(sound);
 	}
+}
+
+///////////////////////////////// AUDIO OBJECT //////////////////////////////////////////
+
+#pragma pack(push)
+#pragma pack(1)
+
+#define RIFF_STR(a, b, c, d) (((u32)(a) << 0) | ((u32)(b) << 8) | ((u32)(c) << 16) | ((u32)(d) << 24))
+
+#define RiffStr_RIFF RIFF_STR('R', 'I', 'F', 'F')
+#define RiffStr_WAVE RIFF_STR('W', 'A', 'V', 'E')
+#define RiffStr_fmt RIFF_STR('f', 'm', 't', ' ')
+#define RiffStr_data RIFF_STR('d', 'a', 't', 'a')
+#define RiffStr_fact RIFF_STR('f', 'a', 'c', 't')
+
+typedef struct {
+	u32 riff;
+	u32 file_size;
+	u32 wave;
+} WaveHeader;
+
+typedef struct {
+	u16 format_tag;
+	u16 channel_count;
+	u32 samples_per_second;
+	u32 avg_bytes_per_second;
+	u16 block_align;
+	u16 bits_per_sample;
+	u16 extension_size;
+	u16 valid_bits;
+	u32 channel_mask;
+	u8 sub_format[16];
+} WaveFmt;
+
+#pragma pack(pop)
+
+b8 audio_load(Audio* audio, const char* filepath)
+{
+	Deserializer s;
+	u8* data;
+	u32 size;
+
+	b8 res = TRUE;
+
+	if (file_read_binary(filepath, &data, &size)) {
+
+		deserializer_begin_buffer(&s, data, size);
+
+		WaveHeader header;
+		deserializer_read(&s, &header, sizeof(WaveHeader));
+
+		if (header.riff != RiffStr_RIFF || header.wave != RiffStr_WAVE || header.file_size != size - 8) {
+			SV_LOG_ERROR("That's an invalid .wav format '%s'\n", filepath);
+			goto corrupted;
+		}
+
+		WaveFmt* fmt;
+		b8 read_fmt = FALSE;
+		void* sample_data = NULL;
+		u32 sample_bytes = 0;
+
+		while (s.cursor < size) {
+
+			u32 chunk_id;
+			u32 chunk_size;
+
+			deserialize_u32(&s, &chunk_id);
+			deserialize_u32(&s, &chunk_size);
+
+			u32 begin_offset = s.cursor;
+
+			switch (chunk_id)
+			{
+
+			case RiffStr_fmt:
+			{
+				fmt = (WaveFmt*)(s.data + s.cursor);
+
+				if (fmt->format_tag == 1) {
+
+					if (fmt->channel_count < 1 || fmt->channel_count > 2) {
+						SV_LOG_ERROR("Invalid channel count %u in '%s'\n", fmt->channel_count, filepath);
+						break;
+					}
+
+					read_fmt = TRUE;
+				}
+				else {
+					SV_LOG_ERROR("Only the PCM format is supported\n");
+				}
+
+			}
+			break;
+
+			case RiffStr_data:
+			{
+				sample_data = s.data + s.cursor;
+				sample_bytes = chunk_size;
+			}
+			break;
+
+			}
+
+
+			begin_offset += chunk_size;
+			if (chunk_size & 1)
+				begin_offset++;
+			s.cursor = begin_offset;
+		}
+
+		if (!read_fmt) {
+			SV_LOG_ERROR("A valid fmt block is not found in '%s'\n", filepath);
+			goto corrupted;
+		}
+		if (sample_bytes == 0) {
+			SV_LOG_ERROR("There are no samples in '%s'\n", filepath);
+			goto corrupted;
+		}
+
+		{
+			audio->channel_count = fmt->channel_count;
+
+			u32 sample_count = (sample_bytes / (fmt->bits_per_sample / 8)) / fmt->channel_count; // Per channel sample count
+			u32 per_channel_bytes = sizeof(i16) * sample_count;
+
+			audio->sample_count = sample_count;
+			audio->samples_per_second = fmt->samples_per_second;
+
+			// TODO: Support 8 bits samples
+
+			if (audio->channel_count == 1) {
+				audio->samples[0] = memory_allocate(per_channel_bytes);
+				audio->samples[1] = NULL;
+
+				deserializer_read(&s, audio->samples[0], per_channel_bytes);
+			}
+			else if (audio->channel_count == 2) {
+				u8* mem = memory_allocate(per_channel_bytes * 2);
+				audio->samples[0] = (i16*)mem;
+				mem += per_channel_bytes;
+				audio->samples[1] = (i16*)mem;
+
+				i16* src = (i16*)sample_data;
+				foreach(i, sample_count) {
+
+					audio->samples[0][i] = *src++;
+					audio->samples[1][i] = *src++;
+				}
+			}
+		}
+
+		goto free_data;
+	corrupted:
+		res = FALSE;
+
+	free_data:
+		deserializer_end_file(&s);
+		memory_free(data);
+	}
+	else res = FALSE;
+
+	return res;
+}
+
+void audio_destroy(Audio* audio)
+{
+	if (audio == NULL)
+		return;
+
+	if (audio->samples[0])
+		memory_free(audio->samples[0]);
+}
+
+void audio_play()
+{
+	AudioInstance* inst = &sound->instance;
+	inst->audio = &sound->audio;
+	inst->begin_sample_index = sound->sample_index;
+
+	f32 seconds = (f32)inst->audio->sample_count / (f32)inst->audio->samples_per_second;
+
+	inst->end_sample_index = sound->sample_index + (u32)(seconds * (f32)sound->samples_per_second);
 }
