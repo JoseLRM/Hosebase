3e0ff7ea68d1a8de82385f4f47f5ce132b110926
Jose <joselrm890@gmail.com>
2022-03-04 07:27:29 +0100

    Some macros

--- defines.h
+++ defines.h
@@ -66,6 +66,8 @@ constexpr f64	f64_max = std::numeric_limits<f64>::max();*/
 #define foreach(_it, _end) for (u32 _it = 0u; _it < (u32)(_end); ++_it)
 #define SV_MIN(a, b) ((a < b) ? a : b)
 #define SV_MAX(a, b) ((a > b) ? a : b)
+#define SV_CLAMP(a, min, max) (SV_MAX(SV_MIN(a, max), min))
+#define SV_POW2(x) ((x) * (x))
 #define SV_STRING(x) #x
 #define SV_ARRAY_SIZE(x) (sizeof(x) / sizeof(*x))
 #define SV_CHECK(x) if (!(x)) return FALSE
